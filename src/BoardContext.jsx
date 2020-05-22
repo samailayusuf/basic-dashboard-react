973c0b95b7dde57004a143562b718bdba526d318
samailayusuf <yusuf7samaila@gmail.com>
2020-05-22 10:33:38 +0100

    creating context xlass

--- src/BoardContext.jsx
+++ src/BoardContext.jsx
@@ -4,6 +4,9 @@ class BoardContext extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            brightTheme:true,
+            lightMode: {fontColor:"#5c5c5c", backgroundMode: "#dbdbdb", buttonMode:"#a3a3a3"},
+            darkMode: {fontColor:"#dbdbdb", backgroundMode:"#5c5c5c", buttonMode:"#5c5858"}
         }
     }
 
