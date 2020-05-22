173177a04a2592333403ef97d5fb0e89ffc266b5
samailayusuf <yusuf7samaila@gmail.com>
2020-05-22 10:53:32 +0100

    creating context xlass

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Board from './Board'
 import Nav from './Nav'
 import Footer from './Footer'
 import "bootstrap/dist/css/bootstrap.min.css/"
+import BoardContextProvider from './BoardContext'
+
 class App extends Component {
     render() { 
         return ( 
             <div>
+                <BoardContextProvider>
                 <Nav/>
                 <Board />
                 <Footer/>
+                </BoardContextProvider>
             </div>
          );
     }
